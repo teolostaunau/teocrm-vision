a29c28e02cb8ed3eec1cf9a616ede5931381b0ac
Teo Lostaunau <teo.lostaunau@gmail.com>
2026-07-09 14:40:11 -0500

    feat(growth-center): complete application layer

---
+++ src/vision/workspaces/growth-center/application/index.ts
@@ -0,0 +1,18 @@
+/**
+ * -----------------------------------------------------------------------------
+ * TeoCRM Vision UI
+ * Growth Center™
+ * -----------------------------------------------------------------------------
+ * Application
+ *
+ * Public exports for the Growth Center application layer.
+ * -----------------------------------------------------------------------------
+ */
+
+export { growthCenterDemoScenario } from "./scenarios/growth-center.scenario";
+
+export { createGrowthCenterWorkspace } from "./factory/growth-center.factory";
+
+export { presentGrowthCenterWorkspace } from "./presenters/growth-center.presenter";
+
+export type { GrowthCenterViewModel } from "./viewmodels/growth-center.viewmodel";
