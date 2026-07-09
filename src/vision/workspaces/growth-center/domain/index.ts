7f228c9aa9d26a10045e8f4cb0914a19a19d6f8d
Teo Lostaunau <teo.lostaunau@gmail.com>
2026-07-09 14:18:53 -0500

    feat(growth-center): complete domain layer

---
+++ src/vision/workspaces/growth-center/domain/index.ts
@@ -0,0 +1,30 @@
+/**
+ * -----------------------------------------------------------------------------
+ * TeoCRM Vision UI
+ * Growth Center™
+ * -----------------------------------------------------------------------------
+ * Domain
+ *
+ * Public exports for the Growth Center domain.
+ * -----------------------------------------------------------------------------
+ */
+
+export * from "./ids";
+
+export * from "./shared";
+
+export * from "./commands";
+
+export * from "./narrative";
+
+export * from "./insight";
+
+export * from "./focus";
+
+export * from "./business";
+
+export * from "./awareness";
+
+export * from "./productivity";
+
+export * from "./workspace";
